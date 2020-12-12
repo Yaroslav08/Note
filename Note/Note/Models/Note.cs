994117e1cc580fe7ab153b1ecb84c0c0ec0c72a6
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-12 12:32:46 +0200

    Added Note entity

---
+++ Note/Note/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Note.Models
+{
+    public class Note
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+        public string FileId { get; set; }
+
+        public bool HasFile() => !string.IsNullOrEmpty(FileId);
+    }
+}
